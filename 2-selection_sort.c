473edcca97a74906fd9dcbd7385e7386b1a6160a
noahwksa <noahwekesa0gmail.com>
2023-09-25 00:19:36 +0300

    correction

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,30 +9,29 @@ void selection_sort(int *array, size_t size)
 {
     size_t i, j, min_idx, tmp;
     int swap_occurred;
-
+    
     if (array == NULL)
-        return;
+    return;
 
-    for (i = 0; i < size; i++)
+for (i = 0; i < size; i++)
+{
+    min_idx = i;
+    swap_occurred = 0;
+    
+    for (j = i + 1; j < size; j++)
     {
-        min_idx = i;
-        swap_occurred = 0;
-
-        for (j = i + 1; j < size; j++)
+        if (array[j] < array[min_idx])
         {
-            if (array[j] < array[min_idx])
-            {
-                min_idx = j;
-                swap_occurred = 1;
+            min_idx = j;
+            swap_occurred = 1;
             }
-        }
-
-        if (swap_occurred)
-        {
-            tmp = array[min_idx];
-            array[min_idx] = array[i];
-            array[i] = tmp;
-            print_array(array, size);
-        }
-    }
+            }
+            if (swap_occurred)
+            {
+                tmp = array[min_idx];
+                array[min_idx] = array[i];
+                array[i] = tmp;
+                print_array(array, size);
+                }
+                }
 }
